b123e57a9daa4bb4a019c010f53f2bdc488cfb05
Matt Halliday <matt.halliday@autologic.com>
2018-06-07 11:30:38 +0100

    Add services.yaml to loader

--- DependencyInjection/AutologicJSONExceptionsExtension.php
+++ DependencyInjection/AutologicJSONExceptionsExtension.php
@@ -2,16 +2,19 @@
 
 namespace Autologic\JSONExceptions\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class AutologicJSONExceptionsExtension extends Extension
 {
     /**
-     * @param array            $configs
+     * @param array $configs
      * @param ContainerBuilder $container
      *
      * @return void
+     * @throws \Exception
      */
     public function load(array $configs, ContainerBuilder $container)
     {
@@ -22,5 +25,8 @@ class AutologicJSONExceptionsExtension extends Extension
             'autologic_json_exceptions.env',
             $container->getParameter('kernel.environment')
         );
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
     }
 }
